d47b5a7837d3ece86180421d9fce8fe792c4919c
ela-codes <teodocio.am@gmail.com>
2024-12-10 14:23:14 -0600

    change Track from class to record

--- backend/src/main/java/ela/project/vibin/model/Track.java
+++ backend/src/main/java/ela/project/vibin/model/Track.java
@@ -2,21 +2,6 @@ package ela.project.vibin.model;
 
 import lombok.Data;
 
-@Data
-public class Track {
-    private String trackName;
 
-    private String artistName;
-
-    private String trackUrl;
-
-    private String artistUrl;
-
-    private String trackLength;
-
-    private boolean explicit;
-
-    private String albumImageUrl;
-
-    private String albumUrl;
+public record Track(String name, String id) {
 }
