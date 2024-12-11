1e313cd4458e3d64f1f888193f831872f08b7217
ela-codes <teodocio.am@gmail.com>
2024-12-11 12:23:35 -0600

    refactor EmotionRepository

--- backend/src/main/java/ela/project/vibin/repository/EmotionRepository.java
+++ backend/src/main/java/ela/project/vibin/repository/EmotionRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EmotionRepository extends JpaRepository<Emotion, UUID> {
     @Query("SELECT e FROM Emotion e WHERE e.emotion = :emotion")
-    Emotion findByEmotion(@Param("emotion") EmotionType emotion);
+    Optional<Emotion> findByEmotion(@Param("emotion") EmotionType emotion);
 
 }
