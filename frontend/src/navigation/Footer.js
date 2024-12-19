2c2d7813090fe316558e9363c09af26b73da8727
ela-codes <teodocio.am@gmail.com>
2024-12-19 09:16:56 -0600

    fix: footer syling

--- frontend/src/navigation/Footer.js
+++ frontend/src/navigation/Footer.js
@@ -2,8 +2,8 @@ export default function Footer() {
     return (
         <footer className="container mt-auto">
             <div id="footerText" className="container d-flex justify-content-center ">
-                <p className="text-white-50 px-4">Last updated Dec 2024</p>
-                <a href="https://github.com/ela-codes" className="text-white-50 px-4">by ela-codes</a>
+                <p className="text-white-50 px-4">Last updated on Dec 2024 by <a href="https://github.com/ela-codes" className="text-white-50">ela-codes</a>.</p>
+                
             </div>
         </footer>
     );
