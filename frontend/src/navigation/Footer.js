c43a2495fbcf2df983aa1a1b77dbf48c12ef0d6f
Ela <103253928+ela-codes@users.noreply.github.com>
2025-01-10 15:39:34 -0600

    chore: project information

--- frontend/src/navigation/Footer.js
+++ frontend/src/navigation/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="container mt-auto">
             <div id="footerText" className="container d-flex justify-content-center ">
-                <p className="text-white-50 px-4">Last updated on Dec 2024 by <a href="https://github.com/ela-codes" className="text-white-50">ela-codes</a>.</p>
+                <p className="text-white-50 px-4">Last updated on Jan 2025 by <a href="https://github.com/ela-codes" className="text-white-50">ela-codes</a>.</p>
                 
             </div>
         </footer>
