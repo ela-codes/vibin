e97c7bf41fa1155b07dd21d4027fde076c70ebe9
ela-codes <elacodes@gmail.com>
2025-01-24 21:09:25 -0600

    feat: beautify about page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+import aboutPageImg from "./style/image-from-rawpixel-id-16837520.png";
+
 
 export default function About() {
 
@@ -23,7 +24,7 @@ export default function About() {
         },
     };
 
-    const pVariants = {
+    const imgVariants = {
         hidden: { opacity: 0, y: 20 },
         visible: {
             opacity: 1,
@@ -32,20 +33,61 @@ export default function About() {
         },
     };
 
+    const pVariant1= {
+        hidden: { opacity: 0, y: 20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { delay: 1, duration: 1.5 },
+        },
+    };
+
+    const pVariant2 = {
+        hidden: { opacity: 0, y: 20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { delay: 2, duration: 1.5 },
+        },
+    };
+
+    const pVariant3 = {
+        hidden: { opacity: 0, y: 20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { delay: 4, duration: 1.5 },
+        },
+    };
+
+    const pVariant4 = {
+        hidden: { opacity: 0, y: 20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { delay: 6, duration: 2 },
+        },
+    };
+
 
     return (
         <motion.div
-            className="container overflow-auto py-3 col-10"
+            className="row overflow-auto py-3 align-items-center justify-content-center"
             initial="hidden"
             animate="visible"
             variants={containerVariants}>
-            <motion.h1 variants={h1Variants}>About Page</motion.h1>
-            <motion.div variants={pVariants} className="small">
-                <p>Typically when we search for songs, we already feel a certain type of way. We know what genres or artists to look for to satisfy that <i>vibe</i>.<br /><strong>VIBIN.</strong> bridges that gap between emotion and music by delivering meaningful song recommendations. </p>
-                <p>Designed to explore API integrations, scalable application design and unit testing, this full stack project is both a creative experiment and a technical showcase for me!</p>
-                <p>Around Dec. 2024, I challenged myself to build a minimum viable product (MVP) in 2-3 weeks, which mimics a typical sprint. I learned about gathering requirements and building a web application without a step-by-step tutorial to follow. I researched, asked my peers for advice, and went through a lot of trial and error. I'm so proud of the outcome!</p>
-                <p>I hope you find joy in a new song you've discovered today~ </p>
+            <motion.h1 variants={h1Variants}>About VIBIN.</motion.h1>
+            <motion.div variants={imgVariants} className='pb-4'>
+            
+                <img src={aboutPageImg} alt="Woman collage cutouts portrait abstract rainbow by rawpixel.com" id="aboutPageImage" />
             </motion.div>
+            <div  className="w-75">
+                <motion.p variants={pVariant1} className='fs-5'><strong>Imagine this.</strong></motion.p>
+                <motion.p variants={pVariant2} >You're the main character of a movie. <br />What music is playing in the background right now?</motion.p>
+                <motion.p variants={pVariant3}><strong>VIBIN.</strong> bridges the gap between emotion and music by delivering meaningful song recommendations. </motion.p>
+                <motion.p  variants={pVariant4} id="aboutPageSubText"><hr />Designed to explore API integrations, scalable application design and unit testing, this full stack project is both a creative experiment and a technical showcase for me! Around Dec. 2024, I challenged myself to build a minimum viable product (MVP) in 2-3 weeks, which mimics a typical sprint. I learned about gathering requirements and building a web application without a step-by-step tutorial to follow. I researched, asked my peers for advice, and went through a lot of trial and error. <br />I'm so proud of the outcome and I hope you find joy in a new tune you've discovered today~
+                </motion.p>
+            </div>
         </motion.div>
 
     );
